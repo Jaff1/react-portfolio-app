4c793c484b1be320f68a17e439551d69dbd60e1d
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-31 10:50:16 +0000

    Updates

---
+++ precache-manifest.16a77df72f8839ee1466b1668bf7c993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "709a8eb86c9d7291b6ac",
+    "url": "/react-portfolio-app/static/js/runtime~main.709a8eb8.js"
+  },
+  {
+    "revision": "1c4692a382b90d5ee977",
+    "url": "/react-portfolio-app/static/js/main.1c4692a3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-portfolio-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1c4692a382b90d5ee977",
+    "url": "/react-portfolio-app/static/css/main.7f9a67b9.chunk.css"
+  },
+  {
+    "revision": "a2cb98052a3dc42cd17b7f43d44b1152",
+    "url": "/react-portfolio-app/index.html"
+  }
+];
