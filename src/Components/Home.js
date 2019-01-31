f90c67c5dc141a1a66ee0aa1c72ba9f7f0db4ea6
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-31 15:56:53 +0000

    added some diagnostic code

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <p>Home Page</p>
+            <p>Home Page - a late change has broken display of other pages!! Caught by not found tho</p>
         </div>
     );
 };
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -4,6 +4,7 @@ export default function NotFound() {
     return (
         <div>
             <h1> 1901311537 Ooops!... the page does not exist :(</h1>
+            <h1>a late change has broken display of sub pages!! Caught by not found tho</h1>
         </div>
     )
 }
