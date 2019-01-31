cc593e12e75c43e8245ca5d581c7c513ff30d867
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-31 14:13:22 +0000

    Added fw page components and a Navigation Thingy coponent as placeholders just to start giving some structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const NewRoute = () => {
-  return (
-    <div>
-      <p> New Route</p>
-    </div>
-  );
-}
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import NotFound from './Components/NotFound'
+import Navigation from './Components/Navigation'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path='/new' component={NewRoute} />
-
-
-
+        <div>
+          <Navigation />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
